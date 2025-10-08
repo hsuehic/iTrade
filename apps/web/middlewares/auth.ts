0ef30daa84dcd57f65b339e6c17008444bf5429b
hsuehic <xiaoweihsueh@gmail.com>
2025-10-08 22:27:34 +0800

    feat: paths to skip auth middleware

--- apps/web/middlewares/auth.ts
+++ apps/web/middlewares/auth.ts
@@ -5,7 +5,7 @@ export function withAuth(request: NextRequest, res: NextResponse) {
   const sessionCookie = getSessionCookie(request);
   const pathname = request.nextUrl.pathname;
   const skipPathsPattern =
-    /(^\/api\/auth\/)|(^\/auth)|(^\/_next)|((\.js|\.css|\.png|\.jpg|\.jpeg|\.svg|\.gif|\.woff|\.woff2|\.ttf|\.eot|\.otf|\.map)$)/;
+    /(^\/api\/mobile)|(^\/api\/auth\/)|(^\/auth)|(^\/_next)|((\.js|\.css|\.png|\.jpg|\.jpeg|\.svg|\.gif|\.woff|\.woff2|\.ttf|\.eot|\.otf|\.map)$)/;
   if (skipPathsPattern.test(pathname)) {
     return res;
   }
