765cb90031d6bfec4b59eeb7e762f2cc7d5612b1
xiaowei <xiaowei.xue@bitdeer.com>
2025-10-23 16:02:28 +0800

    chore: build settings

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-        isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = false
     }
 
     kotlinOptions {
--- apps/mobile/android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ihsueh.itrade
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
