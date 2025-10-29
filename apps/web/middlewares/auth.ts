ee36f4da59f8df00dd9ba6c8ef18e77406f6d949
xiaowei <xiaowei.xue@bitdeer.com>
2025-10-30 02:22:15 +0800

    fix: apple oauth on android devices

--- apps/web/middlewares/auth.ts
+++ apps/web/middlewares/auth.ts
@@ -5,7 +5,7 @@ export function withAuth(request: NextRequest, res: NextResponse) {
   const sessionCookie = getSessionCookie(request);
   const pathname = request.nextUrl.pathname;
   const skipPathsPattern =
-    /(^\/api\/)|(^\/auth)|(^\/_next)|((\.js|\.css|\.png|\.jpg|\.jpeg|\.svg|\.gif|\.woff|\.woff2|\.ttf|\.eot|\.otf|\.map|\.html)$)/;
+    /(^\/api\/)|(^\/auth)|(^\/callbacks)|(^\/_next)|((\.js|\.css|\.png|\.jpg|\.jpeg|\.svg|\.gif|\.woff|\.woff2|\.ttf|\.eot|\.otf|\.map|\.html)$)/;
 
   // Allow landing page for unauthenticated users
   const isLandingPage = pathname === '/' || pathname === '/landing';
