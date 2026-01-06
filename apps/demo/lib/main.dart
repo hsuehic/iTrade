a8486644b36f505c9e8f9cf6c34fcce34f49b75b
xiaowei <xiaowei.xue@bitdeer.com>
2026-01-06 15:39:00 +0800

    chore: remove demo code.

--- apps/demo/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'tree_view_demo.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-// Keep `main.dart` tiny; the demo lives in `tree_view_demo.dart`.
